af59496a95dcbe6c700608407c63243ece3e2fee
DavidPotolski <david@brickabode.com>
2019-12-20 09:15:49 -0300

    Add mp3 sample file that can be played via app

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+typedef void OnError(Exception exception);
+
+void main() {
+  runApp(new MaterialApp(home: new ExampleApp()));
+}
+
+class ExampleApp extends StatefulWidget {
+  @override
+  _ExampleAppState createState() => new _ExampleAppState();
+}
+
+class _ExampleAppState extends State<ExampleApp> {
+  AudioCache audioCache = new AudioCache();
+  AudioPlayer advancedPlayer = new AudioPlayer();
+  String localFilePath;
+
+  Widget _tab(List<Widget> children) {
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: children
+              .map((w) => Container(child: w, padding: EdgeInsets.all(6.0)))
+              .toList(),
+        ),
+      ),
+    );
+  }
+
+  Widget _btn(String txt, VoidCallback onPressed) {
+    return ButtonTheme(
+        minWidth: 48.0,
+        child: RaisedButton(child: Text(txt), onPressed: onPressed));
+  }
+
+  Widget localAsset() {
+    return _tab([
+      Text('Play Local Asset:'),
+      _btn('Play', () => audioCache.play('audio.mp3'))
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 1,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'Mp3 Player'),
+            ],
+          ),
+          title: Text('Athena'),
+        ),
+        body: TabBarView(
+          children: [localAsset()],
+        ),
+      ),
+    );
+  }
+}
