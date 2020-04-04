5dd87bcbd40b5488f603cdc82a8980ec7f091d67
Lucas Lovato <lucaslovato@brickabode.com>
2020-04-04 00:33:44 -0300

    Update on project folder structure

---
+++ lib/views/homePage.dart
@@ -0,0 +1,30 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+import 'package:athena/tabsContent/audioTab.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.audiotrack)),
+              Tab(icon: Icon(Icons.library_books)),
+            ],
+          ),
+          title: Text('Athena'),
+        ),
+        body: TabBarView(
+          children: [
+            AudioTab(),
+            Icon(Icons.library_books),
+          ],
+        ),
+      ),
+    );
+  }
+}
