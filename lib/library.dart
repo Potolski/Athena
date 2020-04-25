8dcd7f4e3a81e158a7fcc1175437134c5a6ee1c5
henry <henryqx@hotmail.com>
2020-04-25 10:02:04 -0300

    WIP: Add mime type identification
    Signed-off-by: henry <henryqx@hotmail.com>

--- lib/library.dart
+++ lib/library.dart
@@ -4,6 +4,7 @@ import 'package:folder_picker/folder_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:mime/mime.dart'
 
 class Library {
   static final Library _instance = Library._internal();
@@ -17,20 +18,29 @@ class Library {
     this.folders = [];
   }
 
+  _addDirectory(Directory folder) {
+
+    folder.list(recursive: true)
+      .where((x) => x is File && lookupMimeType(x.path).startsWith("audio"))
+
+
+  }
+
   addFolders(BuildContext context) async {
     final directory = await getExternalStorageDirectory();
-    print(directory.path);
     final rdir = directory.path
-        .replaceFirst("Android/data/com.example.example/files", "");
-        Map<PermissionGroup, PermissionStatus> permissions =
-        await PermissionHandler().requestPermissions([PermissionGroup.storage]);
+      .replaceFirst("Android/data/com.example.example/files", "");
+    Map<PermissionGroup, PermissionStatus> permissions =
+      await PermissionHandler().requestPermissions([PermissionGroup.storage]);
+
     Navigator.of(context).push<FolderPickerPage>(
         MaterialPageRoute(builder: (BuildContext context) {
-      return FolderPickerPage(
-          rootDirectory: Directory(rdir),
-          /// a [Directory] object
-          action: (BuildContext context, Directory folder) async {
-            print("Picked foldeasddadsdadr $folder");
+          return FolderPickerPage(
+            rootDirectory: Directory(rdir),
+            action: (BuildContext context, Directory folder) async {
+              this.folders.add(folder);
+              print(folder.toString());
+              Navigator.of(context).pop();
           });
     }));
   }
